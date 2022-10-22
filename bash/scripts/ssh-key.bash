458317a3f427ce87918c5cc1dc80a4b598ef654a
NocDevX <xdev.noc@gmail.com>
2022-10-21 22:13:37 -0300

    21/10/2022   - Movidos arquivos bash de setup para repositório separado

--- bash/scripts/ssh-key.bash
+++
@@ -1,9 +0,0 @@
-ssh-keygen -t ed25519
-ssh-agent -s
-ssh-add ~/.ssh/id_ed25519
-
-clear
-
-echo "============= Public Key ==============="
-cat ~/.ssh/id_ed25519.pub
-echo "========================================"
